76fd06dc79639a204539a7e0c39e2515ced4db5e
suchanhan <gkstmcks1225@naver.com>
2021-11-04 15:14:12 +0900

    고침

--- Labs/Lecture06_HandlingInputs/character_runs_esc.py
+++ Labs/Lecture06_HandlingInputs/character_runs_esc.py
@@ -11,8 +11,17 @@ frame = 0
 
 
 def handle_events():
-    # fill here
-    pass
+    global running
+
+    events = get_events()
+    for event in events:  # events 리스트에 담겨 있는 각각에 event를 하나씩 꺼낸다.
+      if event.type == SDL_QUIT:  # 윈도우 x버튼을 눌렀을때 종료
+          running = False
+      elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:  # 어떤 event 하나가 키가 눌렸고 # 그게 ESCAPE면 종료하여라
+          running = False
+
+
+
 
 
 while x < 800 and running:
--- Labs/Lecture07_Linear_Movement/line.py
+++ Labs/Lecture07_Linear_Movement/line.py
@@ -1,6 +1,6 @@
 import turtle
 import random
-
+import math
 
 def stop():
     turtle.bye()
@@ -49,19 +49,51 @@ def draw_point(p):
     turtle.goto(p)
     turtle.dot(5, random.random(), random.random(), random.random())
 
-
+# 직선의 방정식
 def draw_line_basic(p1, p2):
-    # fill here
+    draw_big_point(p1)
+    draw_big_point(p2)
+
+    x1, y1 = p1
+    x2, y2 = p2
+
+    a = (y2 - y1)/(x2 - x1)
+    b = y1 - a * x1
+
+    for x in range(x1, x2+1, 10):
+        y = a*x+b
+        draw_point((x,y))
+
+    draw_point(p2)
     pass
 
 
 def draw_line(p1, p2):
-    # fill here
+
+    draw_big_point(p1)
+    draw_big_point(p2)
+
+    x1, y1 = p1
+    x2, y2 = p2
+
+    for i in range(0, 314, 1):
+        t = i/10
+        a = 160
+        b = 100
+        x = (a-b)*math.cos(t) + b*math.cos(t*(a/b-1))
+        y = (a-b)*math.sin(t) - b*math.sin(t*(a/b-1))
+        draw_point((x, y))
+
+    draw_point(p2)
     pass
 
 
 prepare_turtle_canvas()
 
-# fill here
+p1 = (200, 200)
+p2 = (300, 300)
+
+
+draw_line(p1, p2)
 
 turtle.done()
